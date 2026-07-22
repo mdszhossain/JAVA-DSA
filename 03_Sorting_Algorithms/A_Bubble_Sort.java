232ddeb443108570e5370f2cf06747a962cd7904
mdszhossain <personalmailshazzad@gmail.com>
2026-07-22 18:21:58 +0600

    Bubble Sort Complete

---
+++ 03_Sorting_Algorithms/A_Bubble_Sort.java
@@ -0,0 +1,26 @@
+public class A_Bubble_Sort {
+    public static void bubbleSort(int nums[]) {
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-1-i; j++) {
+                if(nums[j] >= nums[j+1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        printNums(nums);
+    }
+
+    public static void printNums(int nums[]) {
+        for(int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int nums[] = {5, 4, 1, 3, 2};
+        bubbleSort(nums);
+    }
+}
