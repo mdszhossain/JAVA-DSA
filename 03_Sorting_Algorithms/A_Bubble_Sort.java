44022e53678c2f305c4d405f7244f532b19fab78
mdszhossain <personalmailshazzad@gmail.com>
2026-07-22 18:38:15 +0600

    Bubble Sort Updated

--- 03_Sorting_Algorithms/A_Bubble_Sort.java
+++ 03_Sorting_Algorithms/A_Bubble_Sort.java
@@ -1,8 +1,8 @@
 public class A_Bubble_Sort {
     public static void bubbleSort(int nums[]) {
         int n = nums.length;
-        for(int i = 0; i < n-1; i++) {
-            for(int j = 0; j < n-1-i; j++) {
+        for(int i = 0; i <= n-2; i++) {
+            for(int j = 0; j <= n-2-i; j++) {
                 if(nums[j] >= nums[j+1]) {
                     int temp = nums[j];
                     nums[j] = nums[j+1];
---
+++ 03_Sorting_Algorithms/B_Selection_Sort.java
@@ -0,0 +1,5 @@
+public class B_Selection_Sort {
+    public static void main(String[] args) {
+        
+    }
+}
