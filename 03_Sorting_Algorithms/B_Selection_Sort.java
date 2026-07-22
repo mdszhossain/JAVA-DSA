5dbadefb7f83e0adb5877c8e11b5739c57875b68
mdszhossain <personalmailshazzad@gmail.com>
2026-07-22 22:17:52 +0600

    Selection Sort Completed

--- 03_Sorting_Algorithms/B_Selection_Sort.java
+++ 03_Sorting_Algorithms/B_Selection_Sort.java
@@ -1,5 +1,28 @@
 public class B_Selection_Sort {
+    public static void selectionSort(int nums[]) {
+        int n = nums.length;
+        for(int i = 0; i <= n-2; i++) {
+            int minPos = i;
+            for(int j = i + 1; j <= n-1; j++) {
+                if(nums[j] < nums[minPos]) {
+                    minPos = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minPos];
+            nums[minPos] = temp;
+        }
+        printNums(nums);
+    }
+    
+    public static void printNums(int nums[]) {
+        for(int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int nums[] = {5, 4, 1, 3, 2};
+        selectionSort(nums);
     }
 }
